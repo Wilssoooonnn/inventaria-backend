2541224d71b819503de77718216997ee0932ebe1
Wilssoooonnn <adamnuralifi11@gmail.com>
2025-11-16 20:25:09 +0700

    feat(model): Add Stock, Movement, Sale, and SaleItem models and relationships

---
+++ app/Models/StockMovement.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class StockMovement extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'product_id',
+        'location_id',
+        'user_id',
+        'type',
+        'quantity_change',
+        'remarks',
+        'related_id'
+    ];
+    public const UPDATED_AT = null; 
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}

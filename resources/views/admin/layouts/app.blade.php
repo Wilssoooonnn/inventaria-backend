b9bd77690c90e6f21a7c5e8853c18523ce6e4357
Wilssoooonnn <adamnuralifi11@gmail.com>
2025-11-25 18:26:35 +0700

    delete: navigation in admin layouts

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -22,17 +22,6 @@
                 </div>
             @endif
 
-            {{-- <nav class="mb-4 flex flex-wrap space-x-3 p-3 bg-gray-100 rounded-lg shadow-inner">
-                <a href="{{ route('admin.dashboard') }}"
-                    class="px-3 py-2 text-sm font-medium text-indigo-600 hover:bg-white rounded-md">Dashboard</a>
-                <a href="{{ route('admin.products.index') }}"
-                    class="px-3 py-2 text-sm font-medium text-gray-700 hover:bg-white rounded-md">Produk & Menu</a>
-                <a href="{{ route('admin.units.index') }}"
-                    class="px-3 py-2 text-sm font-medium text-gray-700 hover:bg-white rounded-md">Satuan Unit</a>
-                <a href="{{ route('admin.recipes.index') }}"
-                    class="px-3 py-2 text-sm font-medium text-gray-700 hover:bg-white rounded-md">Manajemen Resep</a>
-            </nav> --}}
-
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 @yield('content-admin')
             </div>
